36907db4ceebf17e3d812a8e27881b1ca62a20be
yagnesh parekh <yagneshparekh@rocketmail.com>
2021-11-02 14:45:44 -0400

    printing new line char after loop completion

--- spinner2.js
+++ spinner2.js
@@ -1,12 +1,8 @@
 let delay = 100;
 let arr = ['|', '/','-','\\','|','/','-','\\','|']
-for (let i in arr) {
-  if ( Number(i) === arr.length - 1 ) {
-    setTimeout(() => process.stdout.write(`\r${arr[i]} \n`), delay);
-  } else {
-    setTimeout(() => process.stdout.write(`\r${arr[i]}`), delay);
-  }
+for (let val of arr) {
+  setTimeout(() => process.stdout.write(`\r${val}`), delay);
   delay += 200
-}
+};
 
-
+setTimeout(() => process.stdout.write(`\n`), delay)
